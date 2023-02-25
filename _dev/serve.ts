78834af036c1ab42b57dbcc94c94386794c447e4
GitHub <noreply@github.com>
2023-02-25 09:16:06 +0000

    chore(deps): update deno dependencies

--- _dev/serve.ts
+++ _dev/serve.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.173.0/http/mod.ts";
-import { serveDir } from "https://deno.land/std@0.173.0/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.178.0/http/mod.ts";
+import { serveDir } from "https://deno.land/std@0.178.0/http/file_server.ts";
 
 serve((req) => serveDir(req, { enableCors: true }));
